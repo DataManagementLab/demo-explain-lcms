4fd488555fa2f771e3dbb9407e695dfafe755f4b
Oleksandr Havrylov <sashka.havr@gmail.com>
2024-06-18 20:40:05 +0200

    Add evaluation page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { EvaluationPageComponent } from './evaluation-page/evaluation-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainPageComponent }, //
+  { path: 'evaluation', component: EvaluationPageComponent },
+];
