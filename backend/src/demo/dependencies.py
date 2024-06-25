4999fcfe8ada4d40ecd2c02a5308b3748152c3fb
Oleksandr Havrylov <sashka.havr@gmail.com>
2024-06-25 18:33:04 +0200

    Allow BaseExplainer for routes

--- backend/src/demo/dependencies.py
+++ backend/src/demo/dependencies.py
@@ -18,9 +18,6 @@ def get_plan(plan_id: int, ml: Annotated[MLHelper, Depends()]):
 
 
 def get_explainer(explainer_type: ExplainerType, ml: Annotated[MLHelper, Depends()]):
-    if explainer_type == ExplainerType.BASE:
-        raise HTTPException(status_code=status_code.HTTP_422_UNPROCESSABLE_ENTITY, detail="Base explainer is not supported")
-
     return ml.get_explainer(explainer_type)
 
 
