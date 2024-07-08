5164cdde5f429fb37d0e1250578555861d514ec1
Oleksandr Havrylov <sashka.havr@gmail.com>
2024-07-08 03:15:36 +0200

    Change explainers' labels in legend

--- backend/src/evaluation/service.py
+++ backend/src/evaluation/service.py
@@ -6,13 +6,19 @@ from evaluation.utils import float_range
 from ml.service import ExplainerType
 from zero_shot_learned_db.explainers.load import NodeType, ParsedPlan, Plan
 
+explainer_to_string = {
+    ExplainerType.BASE: "Based on the actual runtime",
+    ExplainerType.GRADIENT: "Gradient",
+    ExplainerType.GUIDED_BP: "Guided Backpropagation",
+}
+
 
 def draw_fidelity_score(data: dict[ExplainerType, list[TablesToScore]], dir: str):
     plt.figure()
     max_tables = max([score.table_count for scores in data.values() for score in scores])
     max_score = math.ceil(max([score.score for scores in data.values() for score in scores]))
     for key, scores in data.items():
-        plt.plot([s.table_count for s in scores], [s.score for s in scores], label=key)
+        plt.plot([s.table_count for s in scores], [s.score for s in scores], label=explainer_to_string[key])
     plt.xticks(range(1, max_tables + 1))
     plt.yticks(float_range(1, max_score + 1))
     plt.xlabel("Table count")
@@ -26,7 +32,7 @@ def draw_cost_score(data: dict[ExplainerType, list[TablesToScore]], dir: str):
     plt.figure()
     max_tables = max([score.table_count for scores in data.values() for score in scores])
     for key, scores in data.items():
-        plt.plot([s.table_count for s in scores], [s.score for s in scores], label=key)
+        plt.plot([s.table_count for s in scores], [s.score for s in scores], label=explainer_to_string[key])
     plt.xticks(range(1, max_tables + 1))
     plt.yticks(float_range(0, 2, floats=10))
     plt.xlabel("Table count")
