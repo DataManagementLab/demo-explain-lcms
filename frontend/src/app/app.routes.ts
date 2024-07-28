3ff5c7bd721b12cde0041eb4a260e21064eeb86f
Oleksandr Havrylov <sashka.havr@gmail.com>
2024-07-28 21:27:16 +0200

    Remove evaluation route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
-import { EvaluationPageComponent } from './evaluation-page/evaluation-page.component';
+// import { EvaluationPageComponent } from './evaluation-page/evaluation-page.component';
 
 export const routes: Routes = [
   { path: '', component: MainPageComponent }, //
-  { path: 'evaluation', component: EvaluationPageComponent },
+  // { path: 'evaluation', component: EvaluationPageComponent },
 ];
