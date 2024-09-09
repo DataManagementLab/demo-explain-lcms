c758d9bfe85e98fd12d98776c4747e5e607d665e
Oleksandr Havrylov <sashka.havr@gmail.com>
2024-09-09 22:27:56 +0200

    Add GNNExplainer in frontend

--- frontend/src/app/services/data/explainer-type.ts
+++ frontend/src/app/services/data/explainer-type.ts
@@ -2,6 +2,7 @@ enum ExplainerType {
   actual = 'base_explainer',
   gradient = 'gradient_explainer',
   guidedBackpropagation = 'guided_bp_explainer',
+  gnnExplainer = 'gnn_explainer',
 }
 
 export default ExplainerType;
