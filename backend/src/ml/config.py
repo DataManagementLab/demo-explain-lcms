d1c7ab704d3b2e84d18d59942f879924fab2e390
Oleksandr Havrylov <sashka.havr@gmail.com>
2024-12-08 18:40:42 +0100

    Create dockerfile

--- backend/src/ml/config.py
+++ backend/src/ml/config.py
@@ -2,17 +2,17 @@ from pydantic import BaseModel
 
 
 class MLSettings(BaseModel):
-    base_data_dir: str = ""
+    base_data_dir: str = ".././zero-shot-data"
     dataset_file: str = ""
     validation_base_dir: str = ""
     validate_queries_in_db: bool = False
     limit_plans: int | None = None
-    statistics_file: str = ""
-    zs_model_dir: str = ""
+    statistics_file: str = "statistics_workload_combined.json"
+    zs_model_dir: str = "evaluation/models"
     zs_model_file_name: str = ""
     load_only_first_model_from_runs_config: bool = True
 
-    hyperparameters_file: str = ""
+    hyperparameters_file: str = "zero_shot_learned_db/experiments/tuned_hyperparameters/tune_best_config.json"
 
     device: str = "cpu"
     batch_size: int = 1
